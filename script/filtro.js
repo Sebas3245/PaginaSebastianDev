0efd16823ce3d3e97f3547b93d3f4a34d1004172
Sebastian Lara <jlsra57@misena.edu.co>
2020-01-16 17:03:31 -0500

    Pagina semi completa

---
+++ script/filtro.js
@@ -0,0 +1,75 @@
+$(function()
+{
+    $('.filter').click(function()
+    {
+        $(this).addClass('active').siblings().removeClass('active'); //con siblings selecciono a los hermanos de lo que hice click
+        let valor = $(this).attr('data-nombre');
+        if(valor == 'todos')
+        {
+            $('.cont-work').show('1000');
+        }
+        else
+        {
+            $('.cont-work').not('.'+ valor).hide('1000');
+            $('.cont-work').filter('.'+valor).show('1000');
+        }
+    });
+
+    let equipo = $('#equipo').offset().top,
+        servicio = $('#servicio').offset().top,
+        portafolio = $('#portafolio').offset().top,
+        fiabilidad = $('#fiabilidad').offset().top,
+        contacto = $('#contacto').offset().top;
+    
+    window.addEventListener('resize', function()
+    {
+        equipo = $('#equipo').offset().top,
+        servicio = $('#servicio').offset().top,
+        portafolio = $('#portafolio').offset().top,
+        fiabilidad = $('#fiabilidad').offset().top,
+        contacto = $('#contacto').offset().top;
+    });
+
+    $('#enlace-inicio').on('click', function(e)
+    {
+        e.preventDefault();
+        $('html , body').animate({
+              scrollTop: 0
+        },600);
+    });
+    $('#enlace-equipo').on('click', function(e)
+    {
+        e.preventDefault();
+        $('html , body').animate({
+              scrollTop: equipo-100
+        },600);
+    });
+    $('#enlace-servicio').on('click', function(e)
+    {
+        e.preventDefault();
+        $('html , body').animate({
+              scrollTop: servicio-100
+        },600);
+    });
+    $('#enlace-portafolio').on('click', function(e)
+    {
+        e.preventDefault();
+        $('html , body').animate({
+              scrollTop: portafolio-100
+        },600);
+    });
+    $('#enlace-fiabilidad').on('click', function(e)
+    {
+        e.preventDefault();
+        $('html , body').animate({
+              scrollTop: fiabilidad-100
+        },600);
+    });
+    $('#enlace-contacto').on('click', function(e)
+    {
+        e.preventDefault();
+        $('html , body').animate({
+              scrollTop: contacto-100
+        },600);
+    });
+});
---
+++ script/main.js
@@ -0,0 +1,134 @@
+//VARIABLES
+
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let mover = document.getElementsByClassName('mover');
+let cerrado = true;
+
+//PRE-LOADER
+window.addEventListener('load', function()
+{
+  $('#onload').fadeOut();
+  $('body').removeClass('hidden');
+  menus();
+});
+
+//MENU Y ANIMACIONES CON SCROLL
+window.addEventListener('scroll', function()
+{
+  menus();
+  aparecerElementos();
+});
+
+//HEADER NAV BACKGROUND
+function menus()
+{
+  let desplazamiento_actual = window.pageYOffset;
+
+    if(desplazamiento_actual<=300)
+    {
+      nav.classList.remove('nav2');
+      nav.classList.remove('nav3');
+      nav.className = ('nav1');
+      nav.style.transition='1s';
+      menu.style.top = '80px'
+      abrir.style.color = '#fff';
+      $('.btn-header').css({'color':'white'});
+      $('.btn-contact').css({'color':'rgb(9, 73, 78)','background':'white'});
+    }
+    else if(desplazamiento_actual>300 && desplazamiento_actual<=900)
+    {
+      nav.classList.remove('nav1');
+      nav.classList.remove('nav3');
+      nav.className = ('nav2');
+      menu.style.top = '100px';
+      abrir.style.color = '#000';
+      $('.btn-header').css({'color':'black'});
+      $('.btn-contact').css({'color':'white','background':'black'});
+    }
+    else
+    {
+      nav.classList.remove('nav1');
+      nav.classList.remove('nav2');
+      nav.className = ('nav3');
+      menu.style.top = '100px';
+      abrir.style.color = '#000';
+      $('.btn-header').css({'color':'black'});
+      $('.btn-contact').css({'color':'white','background':'black'});
+    }
+}
+
+// ANIMACION ELEMENTOS
+function aparecerElementos()
+{
+  let desplazamiento_actual = window.pageYOffset;
+
+  for(var i=0;i<mover.length;i++)
+  {
+    var tipoMov = $(mover[i]).attr('tipo-mov');
+
+    if(desplazamiento_actual<mover[i].offsetTop-700)
+    {
+      mover[i].style.removeProperty('animation');
+      mover[i].style.opacity = '0';
+    }
+
+    if(desplazamiento_actual > mover[i].offsetTop - 600)
+    {
+      mover[i].style.animation = ""+tipoMov+" 1s ease-out";
+      mover[i].style.opacity = '1';
+    }
+  }
+}
+
+// ABRIR MENU RESPONSIVE
+
+abrir.addEventListener('click', function()
+{
+  apertura();
+});
+
+function apertura()
+{
+  if(cerrado)//si es true
+  {
+    menu.style.width = '70vw';
+    cerrado=false;
+  }
+  else
+  {
+    menu.style.width = '0%';
+    menu.style.overflow = 'hidden';
+    cerrado=true;
+  }
+}
+
+//CERRAR MENU RESPONSIVE AL HACER CLICK EN CUALQUIER PARTE
+window.addEventListener('click',function(e)
+{
+  if(cerrado==false)
+  {
+    let span = document.querySelector('span');
+    if(e.target !== span && e.target !== abrir)
+    {
+      menu.style.width = '0%';
+      menu.style.overflow = 'hidden';
+      cerrado = true;
+    }
+  }
+});
+
+//CERRAR MENU RESPONSIVE CON RESIZE
+window.addEventListener('resize', function()
+{
+  if(this.screen.width>=700)
+  {
+    cerrado=true;
+    menu.style.removeProperty('overflow');
+    menu.style.removeProperty('width');
+  }
+});
+
+
