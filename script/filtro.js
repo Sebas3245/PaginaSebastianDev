39671b5096eff81b685d546e42269cd4afa35f40
Sebastian Lara <jlsra57@misena.edu.co>
2020-10-01 15:07:29 -0500

    change portafolio

--- script/filtro.js
+++ script/filtro.js
@@ -1,102 +1,160 @@
 //ALMACENAR EN PORTAFLIO
-$(document).ready(function()
-{
+$(document).ready(function () {
   getPortafolio();
 });
 
 var portafolioImg;
 
-function getPortafolio()
-{
-  $.getJSON("script/portafolio.json",function(pjson)
-  {
-    portafolioImg=pjson;
+function getPortafolio() {
+  $.getJSON("script/portafolio.json", function (pjson) {
+    portafolioImg = pjson;
     genPortafolio(portafolioImg);
   });
 }
 
-function genPortafolio(portafolioImg)
-{
-  $.each(portafolioImg.portafolio,function(i,porta) //ciclo
-    {
-    $("#galeria-work").append(`<div class="cont-work `+porta.dataNombre+`">
-                <div class="img-work">
-                    <img src=`+porta.imagen+` alt="">
-                </div>
-                <div class="textos-work">
-                    <a target="_blank" ${ porta.link ? "href='"+porta.link+"'" : ""} >`+porta.titulo+`</a>
-                </div>
-            </div>`);
+var swiper;
+
+function genPortafolio(portafolioImg) {
+  let hash = {};
+  let setImages = portafolioImg.portafolio.filter(port => hash[port.titulo] ? false : hash[port.titulo] = true);
+
+  setImages.map((porta, i) => {
+    $("#galeria-work").append(`<div class="cont-work ` + porta.dataNombre + `">
+      <div class="img-work">
+        <img src="${porta.imagen}" id="${i}" dataTitle="${porta.titulo}" alt="">
+      </div>
+      <div class="textos-work">
+        <a target="_blank" ${porta.link ? "href='" + porta.link + "'" : ""} >` + porta.titulo + `</a>
+      </div>
+    </div>`);
+  });
+
+  //Portafolio click
+  var gallery = document.getElementById('galeria-work').getElementsByTagName('img');
+
+  for (var i = 0; i < gallery.length; i++) {
+    gallery[i].addEventListener('click', function () {
+      var dataTitle = this.getAttribute('dataTitle');
+      
+      let imgs = portafolioImg.portafolio.filter(img => img.titulo === dataTitle);
+
+      if(typeof swiper === 'object')
+      {
+        swiper.destroy(true, false);
+        swiper = undefined;
+        document.getElementById('imagenes-swiper').innerHTML = '';
+      }
+
+      imgs.map((image, i) => {
+        $('#imagenes-swiper').append(`<div class="swiper-slide slide-${i}">
+          <img src="${image.imagen}">
+        </div>`);  
+      });
+
+      swiper = new Swiper('.swiper-container', {
+        effect: 'cube',
+        grabCursor: true,
+        centeredSlides: true,
+        updateOnWindowResize: true,
+        slidesPerView: 'auto',
+
+        //MANEJAR CON FLECHAS
+        keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+        },
+
+        //PAGINACION
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+
+        // NAVEGACION
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+      location.href = "#miModalPort";
     });
+  }
 }
 
+//CERRAR PORTAFOLIO AL HACER CLICK EN CUALQUIER PARTE
+let flexPort = document.getElementById('flexModalPort');
+
+window.addEventListener('click', function (e) {
+  if (e.target == flexPort) {
+    window.location = '#portafolio'
+    swiper.destroy(true, false);
+  }
+});
+
 //FILTRO DE SELECCION PORTAFOLIO
-$(function()
-{
-    $('.filter').click(function()
-    {
-        $(this).addClass('active').siblings().removeClass('active'); //con siblings selecciono a los hermanos de lo que hice click
-        let valor = $(this).attr('data-nombre');
-        if(valor == 'todos')
-        {
-            $('.cont-work').show('1000');
-        }
-        else
-        {
-            $('.cont-work').not('.'+ valor).hide('1000');
-            $('.cont-work').filter('.'+valor).show('1000');
-        }
-    });
+$(function () {
+  $('.filter').click(function () {
+    $(this).addClass('active').siblings().removeClass('active'); //con siblings selecciono a los hermanos de lo que hice click
+    let valor = $(this).attr('data-nombre');
+    if (valor == 'todos') {
+      $('.cont-work').show('1000');
+    }
+    else {
+      $('.cont-work').not('.' + valor).hide('1000');
+      $('.cont-work').filter('.' + valor).show('1000');
+    }
+  });
 
-//FILTRO DE SCROLL DEL MENU
+  //FILTRO DE SCROLL DEL MENU
 
-    /* $('#enlace-inicio').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-              scrollTop: 0
-        },600);
-    });
-    $('#enlace-equipo').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-              scrollTop: $('#equipo').offset().top-100
-        },600);
-    });
-    $('#enlace-servicio').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-              scrollTop: $('#servicio').offset().top-100
-        },600);
-    });
-    $('#enlace-portafolio').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-              scrollTop: $('#portafolio').offset().top-100
-        },600);
-    });
-    $('#enlace-experiencia').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-              scrollTop: $('#experiencia').offset().top-100
-        },600);
-    });
-    $('#enlace-contacto').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-              scrollTop: $('#contacto').offset().top-100
-        },600);
-    });
-    $('#enlace-comenzar').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-              scrollTop: $('#equipo').offset().top-100
-        },600);
-    }); */
+  /* $('#enlace-inicio').on('click', function(e)
+  {
+      e.preventDefault();
+      $('html , body').animate({
+            scrollTop: 0
+      },600);
+  });
+  $('#enlace-equipo').on('click', function(e)
+  {
+      e.preventDefault();
+      $('html , body').animate({
+            scrollTop: $('#equipo').offset().top-100
+      },600);
+  });
+  $('#enlace-servicio').on('click', function(e)
+  {
+      e.preventDefault();
+      $('html , body').animate({
+            scrollTop: $('#servicio').offset().top-100
+      },600);
+  });
+  $('#enlace-portafolio').on('click', function(e)
+  {
+      e.preventDefault();
+      $('html , body').animate({
+            scrollTop: $('#portafolio').offset().top-100
+      },600);
+  });
+  $('#enlace-experiencia').on('click', function(e)
+  {
+      e.preventDefault();
+      $('html , body').animate({
+            scrollTop: $('#experiencia').offset().top-100
+      },600);
+  });
+  $('#enlace-contacto').on('click', function(e)
+  {
+      e.preventDefault();
+      $('html , body').animate({
+            scrollTop: $('#contacto').offset().top-100
+      },600);
+  });
+  $('#enlace-comenzar').on('click', function(e)
+  {
+      e.preventDefault();
+      $('html , body').animate({
+            scrollTop: $('#equipo').offset().top-100
+      },600);
+  }); */
 });
--- script/galeriaCert.js
+++ script/galeriaCert.js
@@ -1,93 +1,81 @@
 //PRE-LOADER
-window.addEventListener('load', function()
-{
+window.addEventListener('load', function () {
   $('#onload').fadeOut();
   $('body').removeClass('hidden');
 });
 
 //ALMACENAR EN GALERIA DE CERTIFICADOS
-$(document).ready(function()
-{
+$(document).ready(function () {
   getCertificados();
 });
 
 var certificadoImg;
 
-function getCertificados()
-{
-  $.getJSON("script/certificados.json",function(cjson)
-  {
-    certificadoImg=cjson;
+function getCertificados() {
+  $.getJSON("script/certificados.json", function (cjson) {
+    certificadoImg = cjson;
     genGaleriaCert(certificadoImg);
   });
 }
 
-function genGaleriaCert(certificadoImg)
-{
-  $.each(certificadoImg.certificados,function(i,cert) //ciclo
-    {
-    $("#gallery-container").append(`<div class="gallery-item mover" tipo-mov=`+cert.animacion+`>
-      <img src=`+cert.imagen+` id=`+i+` class="gallery-img">
-      <span>Realizado: `+cert.fecha+`</span>
+var swiper;
+function genGaleriaCert(certificadoImg) {
+  $.each(certificadoImg.certificados, function (i, cert) //ciclo
+  {
+    $("#gallery-container").append(`<div class="gallery-item mover" tipo-mov=` + cert.animacion + `>
+      <img src=`+ cert.imagen + ` id=` + i + ` class="gallery-img">
+      <span>Realizado: `+ cert.fecha + `</span>
     </div>`);
-    $('#imagenes-swiper').append(`<div class="swiper-slide slide-`+i+`">
-      <img src=`+cert.imagen+`>
+    $('#imagenes-swiper').append(`<div class="swiper-slide slide-` + i + `">
+      <img src=`+ cert.imagen + `>
     </div>`);
   });
 
   //GALERIA
   var gallery = document.getElementById('gallery-container').getElementsByTagName('img');
-  var swiper;
-  for(var i = 0; i < gallery.length;i++ )
-  {
-    gallery[i].addEventListener('click', function()
-    {
-        var id = this.getAttribute('id');
-        console.log(id);
-        swiper = new Swiper('.swiper-container', {
-          effect: 'coverflow',
-          grabCursor: true,
-          centeredSlides: true,
-          initialSlide: id,
-          updateOnWindowResize: true,
-          slidesPerView: 'auto',
-          //loop: true, 
+  for (var i = 0; i < gallery.length; i++) {
+    gallery[i].addEventListener('click', function () {
+      var id = this.getAttribute('id');
 
-          //MANEJAR CON FLECHAS
-          keyboard: {
-            enabled: true,
-            onlyInViewport: false,
-          },
+      swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        initialSlide: id,
+        updateOnWindowResize: true,
+        slidesPerView: 'auto',
+        //loop: true, 
 
-          //PAGINACION
-          pagination: {
-            el: '.swiper-pagination',
-          },
-   
-          // NAVEGACION
-         navigation: {
-           nextEl: '.swiper-button-next',
-           prevEl: '.swiper-button-prev',
-         },
-        });
+        //MANEJAR CON FLECHAS
+        keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+        },
 
-        location.href="#modal";
+        //PAGINACION
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+
+        // NAVEGACION
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+      location.href = "#modal";
     });
+  }
 }
-}
-
-
-
-
 
 
 //CERRAR GALERIA AL HACER CLICK EN CUALQUIER PARTE
 let flex = document.getElementById('flexModal');
 
-window.addEventListener('click', function(e)
-{
-  if(e.target == flex)
-  {
+window.addEventListener('click', function (e) {
+  if (e.target == flex) {
     window.location = '#'
     swiper.destroy(true, false);
   }
@@ -95,45 +83,38 @@ window.addEventListener('click', function(e)
 
 
 // ANIMACION 
-window.addEventListener('load', function()
-{
+window.addEventListener('load', function () {
   aparecerElementos();
 });
 
-window.addEventListener('scroll', function()
-{
+window.addEventListener('scroll', function () {
   aparecerElementos();
 });
 
 let mover = document.getElementsByClassName('mover');
 
-function aparecerElementos()
-{
+function aparecerElementos() {
   let desplazamiento_actual = window.pageYOffset;
 
-  for(var i=0;i<mover.length;i++)
-  {
+  for (var i = 0; i < mover.length; i++) {
     var tipoMov = $(mover[i]).attr('tipo-mov');
 
-    if(desplazamiento_actual<mover[i].offsetTop-700)
-    {
+    if (desplazamiento_actual < mover[i].offsetTop - 700) {
       mover[i].style.removeProperty('animation');
       mover[i].style.opacity = '0';
     }
 
-    if(desplazamiento_actual > mover[i].offsetTop - 500)
-    {
-      mover[i].style.animation = ""+tipoMov+" 1s ease-out";
+    if (desplazamiento_actual > mover[i].offsetTop - 500) {
+      mover[i].style.animation = "" + tipoMov + " 1s ease-out";
       mover[i].style.opacity = '1';
     }
   }
 }
 
 //SCROLL COMENZAR
-$('#enlace-comenzar').on('click', function(e)
-    {
-        e.preventDefault();
-        $('html , body').animate({
-          scrollTop: $('.gallery-item').offset().top-20
-        },600);
-    });
+$('#enlace-comenzar').on('click', function (e) {
+  e.preventDefault();
+  $('html , body').animate({
+    scrollTop: $('.gallery-item').offset().top - 20
+  }, 600);
+});
